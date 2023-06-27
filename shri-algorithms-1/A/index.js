fb5035236ebb32e68a1e54f052db8f90118d61b3
pavel@else <degtiarenko.p@finfort.ru>
2023-06-27 16:18:45 +0300

    A

--- shri-algorithms-1/A/index.js
+++ shri-algorithms-1/A/index.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 let content = fs.readFileSync('input.txt', 'utf8');
 const [n] = content.toString().split('\n');
 
+let t = n;
+let r = 0;
 
+while (r < t) {
+    r += 1;
+    t -= r;
+}
 
-fs.writeFileSync('output.txt', (n).toString());
+fs.writeFileSync('output.txt', (r).toString());
