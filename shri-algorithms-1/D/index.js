34fd471e6b1d790925b469b776f5dc747ad0f682
pavel@else <degtiarenko.p@finfort.ru>
2023-07-04 15:44:02 +0300

    D

--- shri-algorithms-1/D/index.js
+++ shri-algorithms-1/D/index.js
@@ -1,33 +1,29 @@
+// Валится тест №3. Я не знаю, как его побеждать. Тестовые примеры обрабатываются корректно.
 const fs = require('fs');
 
 let content = fs.readFileSync('input.txt', 'utf8');
 const [n, timeString] = content.toString().split('\n');
+const day = 24 * 60;
 
 if (n <= 1) {
-    fs.writeFileSync('output.txt', '0');
+    fs.writeFileSync('output.txt', day.toString());
     return;
 }
 
 const times = timeString.trimEnd().split(' ');
 
 const format = (str) => {
-    const [HH, MM] = str.split(':');
-    return +HH * 60 + +MM;
+    const [h, m] = str.split(':');
+    return +h * 60 + +m;
 };
 
 
 let sorted = times.map(format).sort();
 
-const day = 24 * 60;
-
 let min = day - sorted.at(-1) + sorted[0];
 
-for (let i = 0, j = 1; j < n; j += 1, i += 1) {
-    const diff = sorted[j] - sorted[i];
-    if (!min) {
-        min = diff;
-        continue;
-    }
+for (let i = 1; i < n; i += 1) {
+    const diff = sorted[i] - sorted[i - 1];
 
     if (diff < min) {
         min = diff;
